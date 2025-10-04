fd35606b8382bc43d5a82c3657d42fbc65a4a999
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-10-04 08:36:21 +0330

    Message Tag Color

--- Ticketing/settings.py
+++ Ticketing/settings.py
@@ -124,12 +124,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# from django.contrib.messages import constants as messages
-#
-# MESSAGE_TAGS = {
-#     messages.DEBUG: 'secondary',
-#     messages.INFO: 'info',
-#     messages.SUCCESS: 'success',
-#     messages.WARNING: 'warning',
-#     messages.ERROR: 'danger',
-# }
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'secondary',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
