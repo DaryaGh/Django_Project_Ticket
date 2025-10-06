4b6ab01e8d8cf1bebed7341957e0d8da9a19add3
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-10-06 18:04:12 +0330

    Feat : make EditConfirm.js to test and comment in project

---
+++ Tickets/templates/Js/EditConfirm.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function (){
+    const editBox = document.querySelectorAll('.edit-confirm');
+
+    editBox.forEach(link => link.addEventListener('click',function (e){
+        e.preventDefault();
+        const editConfirmation = confirm("Are you sure to UPDATE this Ticket ???");
+
+        if (editConfirmation){
+            window.location.href = this.href
+        }
+    }))
+})
