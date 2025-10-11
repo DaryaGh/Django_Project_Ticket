3dee80e7a6f3870d324163332e76250dd10d99ce
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-10-11 11:14:45 +0330

    Feat : move row

--- Tickets/forms.py
+++ Tickets/forms.py
@@ -1,27 +1,28 @@
 from django import forms
 from Tickets.models import Ticket
 
-class TicketForm(forms.ModelForm):
 
+class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['contact_name',
-            'contact_email',
-            'contact_phone',
-            'department',
-            'category',
-            'priority',
-            'subject',
-            'description',
-            'tags',
-            'max_replay_date',
-            'due_date',
+                  'contact_email',
+                  'contact_phone',
+                  'department',
+                  'category',
+                  'priority',
+                  'subject',
+                  'max_replay_date',
+                  'tags',
+                  'description',
+                  'due_date',
                   ]
         exclude = ['created_by', 'closed_at', 'tracking_code', 'status']
 
         widgets = {
             'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Subject ...'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Describe your Issue ...'}),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Describe your Issue ...'}),
             'priority': forms.Select(attrs={'class': 'form-select'}),
             'category': forms.Select(attrs={'class': 'form-select'}),
             'tags': forms.SelectMultiple(attrs={'class': 'form-select'}),
@@ -29,7 +30,9 @@ class TicketForm(forms.ModelForm):
             'due_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
             'contact_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your full name'}),
             'contact_email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'your@email.com'}),
-            'contact_phone': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Your phone number','maxlength': '11','pattern': '[0-9]{10,11}','title': 'Please enter 10 or 11 digits'}),
+            'contact_phone': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Your phone number', 'maxlength': '11',
+                       'pattern': '[0-9]{10,11}', 'title': 'Please enter 10 or 11 digits'}),
             'department': forms.Select(attrs={'class': 'form-select'}),
         }
         labels = {
