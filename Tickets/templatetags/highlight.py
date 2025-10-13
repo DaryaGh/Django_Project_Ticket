94a6fd6b2b9c2e2613c7ca073e436eeb5ce71522
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-10-13 09:45:59 +0330

    Feat : Make Templatetags

---
+++ Tickets/templatetags/highlight.py
@@ -0,0 +1,16 @@
+from django import template
+import re
+
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def highlight(text, search):
+    if not search:
+        return text
+    pattern = re.compile(re.escape(search),re.IGNORECASE)
+    return mark_safe(pattern.sub(
+        lambda m: f'<mark>{m.group(0)}</mark>',
+        str(text)
+    ))
