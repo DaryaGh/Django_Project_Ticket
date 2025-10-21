1ef0659b7bc146c192b482130a9268f3d498a192
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-10-21 09:49:56 +0330

    Components ==> Toast

--- Tickets/templates/Js/toast.js
+++ Tickets/templates/Js/toast.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded",function (){
-        let toastElList =[].slice.call(document.querySelectorAll('.toast'))
-        toastElList.map(function (toastEl){
-            let toast = new bootstrap.Toast(toastEl)
-            toast.show()
-        })
-    })
+document.addEventListener("DOMContentLoaded", function () {
+    const toastElList = document.querySelectorAll('.toast');
+    toastElList.forEach((toastEl, index) => {
+        const toast = new bootstrap.Toast(toastEl, {
+            autohide: true,
+            delay: 2000
+        });
+        toast.show();
+        toastEl.addEventListener('hidden.bs.toast', function () {
+            this.remove();
+        });
+    });
+});
