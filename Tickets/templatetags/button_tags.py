54acd060b9730a34f4681f1d52032ba59473b7de
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-10-21 09:57:48 +0330

    Component ==> Add Outline / Modal / Attr

--- Tickets/templatetags/button_tags.py
+++ Tickets/templatetags/button_tags.py
@@ -8,41 +8,67 @@ register = template.Library()
 def button(label="Ok",
            url=None,
            color='primary',
-           # outline=True,
            icon=None,
            type='link',
            size='sm',
-           extra_class ='',
-           confirm=None):
+           extra_class='',
+           confirm=None,
+           modal_target=None,
+           outline=False,
+           **attrs):
     """
     usage:
-        {% button 'Edit' ticket.get_edit_url color='warming' icon='bi-pencil'%}
-        {% button 'save' type='submit' color='primary' icon='bi-check-lg'%}
-        {% button "delete" ticket.get_delete_url color='danger' icon='bi-trash' confirm=''%}
+        {% button 'Edit' ticket.get_edit_url color='warning' icon='bi-pencil' %}
+        {% button 'save' type='submit' color='primary' icon='bi-check-lg' %}
+        {% button "delete" color='danger' icon='bi-trash3' modal_target='#deleteModal' data_ticket_id=ticket.id data_ticket_number=ticket.id %}
     """
 
-    esc  = conditional_escape
+    esc = conditional_escape
+
+    if outline:
+        outline = f'btn-outline-{color}'
+    else:
+        outline = f'btn-{color}'
 
     size_cls = f' btn-{size}' if size in ('sm', 'lg') else ''
 
-    if color and color not in ('primary', 'secondary','dark','light','danger','info','success', 'warning'):
+    if color and color not in ('primary', 'secondary', 'dark', 'light', 'danger', 'info', 'success', 'warning'):
         color = 'primary'
 
-    cls = f'btn btn-{esc(color)}{size_cls} {esc(extra_class)}'.strip()
-
-    # چون تگ هست و بخواهیم به صورت رشته در بیاریم
+    cls = f'btn {outline}{size_cls} {esc(extra_class)}'.strip()
+    #     # چون تگ هست و بخواهیم به صورت رشته در بیاریم
     icon_html = ''
     if icon:
         icon_html = format_html('<i class="bi {} me-1"></i>', esc(icon))
 
+    attrs_html = ''
+    for key, value in attrs.items():
+        attr_name = key.replace('_', '-')
+        attrs_html += format_html(' {}="{}"', esc(attr_name), esc(value))
+
+    modal_attrs = ''
+    if modal_target:
+        modal_attrs = format_html(' data-bs-toggle="modal" data-bs-target="{}"', esc(modal_target))
+
     if url and type == 'link':
-        return format_html('<a href="{}" class="{}">{}</a>',esc(url),cls,mark_safe(icon_html + esc(label)))
-
-    else:
-
         confirm_attr = ''
         if confirm:
-            confirm_js = f'return confirm({format_html("{}", esc(confirm))});'
-            confirm_attr = format_html('onclick="{}"',mark_safe(confirm_js))
+            confirm_js = f'return confirm("{esc(confirm)}");'
+            confirm_attr = format_html(' onclick="{}"', mark_safe(confirm_js))
+
+        return format_html(
+            '<a href="{}" class="{}"{}{}{}>{}</a>',
+            esc(url), cls, confirm_attr, modal_attrs, mark_safe(attrs_html), mark_safe(icon_html + esc(label))
+        )
+
+    else:
+        confirm_attr = ''
+        if confirm:
+            confirm_js = f'return confirm("{esc(confirm)}");'
+            confirm_attr = format_html(' onclick="{}"', mark_safe(confirm_js))
+
         btn_type = esc(type) if type else 'submit'
-        return format_html('<button type="{}" class="{}"{}>{}</button>', btn_type,cls,confirm_attr,mark_safe(icon_html + esc(label)))
+        return format_html(
+            '<button type="{}" class="{}"{}{}{}>{}</button>',
+            btn_type, cls, confirm_attr, modal_attrs, mark_safe(attrs_html), mark_safe(icon_html + esc(label))
+        )
