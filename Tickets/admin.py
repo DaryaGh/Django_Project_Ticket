54576fe638bf58926cce734ad5c23d35d5551034
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-11-15 08:33:06 +0330

    Feat : Ticket model registration in admin

--- Tickets/admin.py
+++ Tickets/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
 from Tickets.models import *
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
-admin.site.register(LogSearch)
-admin.site.register(Ticket)
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('subject',)
+    fieldsets = (
+        (None,{
+        'fields': ('subject','description','category'),
+    }),
+        ("Timeline",{
+            "fields":("max_replay_date","closed_at")
+        }),
+        ("User Info",{
+            "fields":("created_at","updated_at")
+        }),
+    )
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('assignee_id','seen_at','status')
+
+admin.site.register(SearchLogSignal)
+# admin.site.register(Ticket)
 admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Assignment)
+# admin.site.register(Tag)
+# admin.site.register(Assignment)
+
+# راه دوم برای ساخت logSearch
+# admin.site.register(LogSearch)
