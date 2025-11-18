359ffe35c0c5786e4ebae2bf3f16f421cb92016e
Darya Ghaffari <daryaaa.ghffary@gmail.com>
2025-11-18 11:45:23 +0330

    Feat : Add to Templatetags=> RESPONSE_STATUS_CHOICES/COLOR - DEPARTMENT_COLORS

--- Tickets/templatetags/custom_filters.py
+++ Tickets/templatetags/custom_filters.py
@@ -1,11 +1,23 @@
 from django import template
-from Tickets.Choices import PRIORITY_COLORS
+from Tickets.Choices import *
 
 register = template.Library()
 
 @register.filter
 def get_priority_color(priority):
     return PRIORITY_COLORS.get(priority, '#6c757d')
+
+@register.filter
+def get_status_color(status):
+    return STATUS_COLORS.get(status, '#6c757d')
+
+@register.filter
+def get_department_color(department):
+    return DEPARTMENT_COLORS.get(department, '#6c757d')
+
+@register.filter
+def get_response_status_color(response_status):
+    return RESPONSE_STATUS_COLORS.get(response_status, '#6c757d')
 
 @register.filter
 def get_item(dictionary, key):
